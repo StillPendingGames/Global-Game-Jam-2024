bdb90e25db2d1947d1300b4f6ff51ddd72b06f78
MetalTug <wguiles@mail.bradley.edu>
2024-01-23 23:16:13 -0600

    updates to water scene

---
+++ Assets/Water Effect/TestWaterSpout.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWaterSpout : MonoBehaviour
+{
+    public GameObject waterParticle;
+    public GameObject originPoint;
+    public float fireRate;
+    public float thrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(shootWater());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator shootWater()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(fireRate);
+            ShootWater();
+        }
+        
+    }
+
+    public void ShootWater()
+    {
+        GameObject g = Instantiate(waterParticle, originPoint.transform.position, originPoint.transform.rotation);
+        g.GetComponent<Rigidbody2D>().AddForce(g.transform.up * thrust);
+        //push it... up?
+    }
+}
