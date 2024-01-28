b9fe889bb15315b6e38db34fc9da354496821480
MetalTug <wguiles@mail.bradley.edu>
2024-01-28 14:51:30 -0600

    Objects added to tutorial level;; win screen added

--- Assets/Scripts/Tutorials/TutorialTarget.cs
+++ Assets/Scripts/Tutorials/TutorialTarget.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TutorialTarget : MonoBehaviour
 {
     public float health;
+    public GameObject waterParticle;
     
     
     void OnTriggerEnter2D(Collider2D collision)
@@ -13,6 +14,7 @@ public class TutorialTarget : MonoBehaviour
         if (collision.gameObject.tag == "water")
         {
             SimpleObjectPool.Despawn(collision.gameObject);
+            GameObject g = Instantiate(waterParticle, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
             health--;
 
             if (health <= 0)
